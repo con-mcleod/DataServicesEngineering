505378860a26974d490ed9a50dc5d97125a91ee4
Connor McLeod <z5058240@ad.unsw.edu.au>
2018-08-21 22:16:10 +1000

    Just needs proofread/commenting/final touches now

--- Ass1/ass1.py
+++ Ass1/ass1.py
@@ -8,12 +8,18 @@ Note to assessor:
 Some key things to look out for:
 	- I have removed the "totals" row from the dataframe 
 	- I have renamed all of the column headings
-	- Removing the 'Rubish' column is a permanent change
-	- Removing rows containing a NaN value is a permanent change
-
+	- Removing the 'Rubish' column was a permanent change
+	- Removing rows containing a NaN value was a permanent change
+	- My matplotlib errors on my OS without the additional line below the import 
+		- please remove this line if it bugs out your machine (line 20)
 """
 
 import pandas as pd
+
+import matplotlib as mpl
+mpl.use('TkAgg')							# may need to remove this line
+import matplotlib.pyplot as plt
+
 
 
 
@@ -128,6 +134,8 @@ def question_4(df):
 
 
 def question_5(df):
+	"""
+	"""
 	# remove commas from summer gold medal tally and convert to integers
 	df['S_Gold'] = [int(val.replace(',','')) for val in df['S_Gold']]
 	df = df.sort_values('S_Gold', ascending=False)
@@ -135,6 +143,8 @@ def question_5(df):
 
 
 def question_6(df):
+	"""
+	"""
 	# remove commas from winter gold medal tally and convert to integers
 	df['W_Gold'] = [int(val.replace(',','')) for val in df['W_Gold']]
 	df['Gold_Difference'] = abs(df['S_Gold'] - df['W_Gold'])
@@ -143,16 +153,52 @@ def question_6(df):
 
 
 def question_7(df):
-	pass
+	"""
+	"""
+	# remove commas from combined gold medal tally and convert to integers
+	df['C_Total'] = [int(val.replace(',','')) for val in df['C_Total']]
+	df = df.sort_values('C_Total', ascending=False)
+	print_df(df,True,True,5)
+	df_7 = df.tail(5)
+	print_df(df_7,False,True,False)
+	return df
 
 
-def question_8():
-	pass
+def question_8(df):
+	"""
+	"""
+	df['Team'] = [val.split('(')[0].lstrip().strip() for val in df['Team']]
+	df_8 = df.set_index('Team')
+	df_8['W_Total'] = [int(val.replace(',','')) for val in df['W_Total']]
+	df_8['S_Total'] = [int(val.replace(',','')) for val in df['S_Total']]
+	df_8 = df_8.head(10)
 
 
-def question_9():
-	pass
+	df_8[['W_Total','S_Total']].plot(kind='barh', stacked=True)
+	plt.xlabel('Total Medal Tally')
+	plt.ylabel('Country')
+	plt.title('Summer/Winter Medal Split for 10 Highest Medal Winners')
+	plt.tight_layout()
+	plt.show()
 
+
+def question_9(df):
+	"""
+	"""
+	df['Team'] = [val.split('(')[0].lstrip().strip() for val in df['Team']]
+	df_9 = df
+	df_9['W_Silver'] = [int(val.replace(',','')) for val in df['W_Silver']]
+	df_9['W_Bronze'] = [int(val.replace(',','')) for val in df['W_Bronze']]
+	df_9 = df_9.loc[df_9['Team'].isin(['United States','Australia','Great Britain','Japan','New Zealand'])]
+	df_9 = df_9.set_index('Team')
+
+	df_9[['W_Gold','W_Silver','W_Bronze']].plot(kind='bar')
+	plt.xlabel('Country')
+	plt.ylabel('Medal Tally')
+	plt.title('Gold/Silver/Bronze Split for 5 Countries')
+	plt.tight_layout()
+	plt.show()
+	plt.show()
 
 
 
@@ -198,13 +244,15 @@ if __name__ == '__main__':
 
 	# Q7
 	print ("\nQuestion 7:\n")
-	question_7(df)
+	df = question_7(df)
 
 	# Q8
 	print ("\nQuestion 8:\n")
+	question_8(df)
 
 	# Q9
 	print ("\nQuestion 9:\n")
+	question_9(df)
 
 	
 
